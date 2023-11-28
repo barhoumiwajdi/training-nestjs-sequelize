3a11cf845d2f5605e924c33e38dc30b446b37173
barhoumiwajdi <wajdi.barhoumi26@gmail.com>
2023-11-28 13:20:28 +0100

    Customize Error Response

--- src/Services/User.service.ts
+++ src/Services/User.service.ts
@@ -26,7 +26,11 @@ export class UserService {
                 })
             }
             else {
-                throw new BadRequestException('User Nor Found', { cause: new Error(), description: 'Some error description' })
+                throw new BadRequestException('User Nor Found',
+                    {
+                        cause: new Error(),
+                        description: 'Some error description'
+                    })
             }
         } catch (error) {
             throw new HttpException({
@@ -47,10 +51,39 @@ export class UserService {
                 return data
             }
             else {
-                throw new BadRequestException('User Nor Found', { cause: new Error(), description: 'Some error description' })
+                throw new BadRequestException('User Nor Found',
+                    {
+                        cause: new Error(),
+                        description: 'Some error description'
+                    })
             }
         }
         catch (error) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: 'This is a custom message Internal Error',
+            }, HttpStatus.INTERNAL_SERVER_ERROR, {
+                cause: error
+            });
+        }
+    }
+    async finByName(name: any) {
+        try {
+            const data = await this.userRepository.findOne({
+                where: { firstname: name }
+            })
+            if (data) {
+                return data
+            }
+            else {
+                throw new BadRequestException('User Not found',
+                    {
+                        cause: new Error(),
+                        description: 'Some error description'
+                    })
+            }
+
+        } catch (error) {
             throw new HttpException({
                 status: HttpStatus.INTERNAL_SERVER_ERROR,
                 error: 'This is a custom message Internal Error',
